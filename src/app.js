3f1fe33f9b0fd65c82f7eaf613f2e8da279a734d
ENT\delcas1 <sergio.f.delcastillo@medtronic.com>
2020-12-10 14:57:14 -0600

    Removed Hardcoded Expenses

--- src/app.js
+++ src/app.js
@@ -10,29 +10,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent bill', amount: 109500 }));
-store.dispatch(addExpense({ description: '123abcs', amount: 109500 }));
-
-//both expenses have the word bill, they shuld show up
-//store.dispatch(setTextFilter('water'));
-
-/* setTimeout(() => {
-    store.dispatch(setTextFilter('bill'));
-}, 3000);   */
- 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-//Provider allows us to provide or make available store, to all
-//the components that make up our application - super usefull, no 
-//need to individually pass comps aroud
-//this here is all it takes to setup the store provider
-//AND: since all of the components now have access to the store
-//we can take advantage of the CONNECT function of Redux
-// STEP 1 - create PROVIDER
 const jsx = (
     <Provider store={store}>
     <AppRouter />
